a2c68618e82b860e402afadf9fb564a0620b0f30
Alex <alecz.fia@gmail.com>
2020-03-31 13:29:07 +0100

    Added exceptions

---
+++ src/Exception/InvalidCardColourException.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Exception;
+
+
+use Throwable;
+
+class InvalidCardColourException extends \Exception
+{
+    /**
+     * InvalidCardColourException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "Invalid card colour provided", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * String representation of the object
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf('%s:%s(%s)', __CLASS__, $this->message, $this->code);
+    }
+}
---
+++ src/Exception/InvalidPlayerException.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Exception;
+
+
+use Throwable;
+
+class InvalidPlayerException extends \Exception
+{
+    /**
+     * InvalidCardShapeException constructor.
+     * @param string $message
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "Invalid player type provided", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * String representation of the object
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf('%s:%s(%s)', __CLASS__, $this->message, $this->code);
+    }
+}
